d18b566eb5582cd8fa8bd0623a5de9b3108812e8
Pave1iOS <PGribachev94@yandex.ru>
2024-08-27 00:08:30 +0300

    борьба с ошибкой декодирования

---
+++ Vodovoz.TestCase/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Vodovoz.TestCase
+//
+//  Created by Pavel Gribachev on 26.08.2024.
+//
+
+import Foundation
+
+final class NetworkManager {
+	static let shared = NetworkManager()
+	
+	private init() {}
+	
+	
+}
---
+++ Vodovoz.TestCase/V/GoodsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GoodsViewModel.swift
+//  Vodovoz.TestCase
+//
+//  Created by Pavel Gribachev on 26.08.2024.
+//
+
+import Foundation
